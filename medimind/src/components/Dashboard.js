6f194b4be281a69743196c87908d54955d4f6aca
SiratSabir78 <siratsabir78@gmail.com>
2025-05-30 11:53:47 +0500

    Changed the UI.

--- medimind/src/components/Dashboard.js
+++ medimind/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import LoginPatients from "./LoginPatients.js";
 import SignUp from "./SignUp.js";
 import Login from "./Login.js";
 import "./Dashboard.css";
-
+import coverimage from "../images/cover.png";
 function Dashboard() {
   const [view, setView] = useState("dashboard");
 
@@ -17,10 +17,16 @@ function Dashboard() {
         return <SignUp />;
       default:
         return (
-          <div className="navbar">
-            <div className="nav-content">
+          <div className="dashboard-container">
+            <div className="navbar-custom">
+              <img
+                src={coverimage}
+                alt="KRL Hospital"
+                style={{ width: "70%" }}
+              />
+              <h2 className="title">Hospital Portal</h2>
               <div className="dropdown">
-                <button className="dropbtn">Portal</button>
+                <button className="dropbtn">Select Portal</button>
                 <div className="dropdown-content">
                   <button onClick={() => setView("patient")}>
                     Login as Patient
