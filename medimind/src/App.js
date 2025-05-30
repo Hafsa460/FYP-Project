2a9405ce0f37c8b3b4299441d7016dd89f62193c
SiratSabir78 <siratsabir78@gmail.com>
2025-05-30 13:31:02 +0500

    added routes

--- medimind/src/App.js
+++ medimind/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import Dashboard from './components/Dashboard';
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginDashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import LoginPatients from "./components/LoginPatients";
+import SignUp from "./components/SignUp";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginDashboard />} />
+      <Route path="/login-doctor" element={<Login />} />
+      <Route path="/login-patient" element={<LoginPatients />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
