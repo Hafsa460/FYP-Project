5f444c409c50b03ae15b50f494bf87ebfb63a0d4
Areeba Khan <areebaqayyumd6@gmail.com>
2025-06-10 20:37:14 +0500

    First commit on Areebas branch

---
+++ medimind/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginDashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import LoginPatients from "./components/LoginPatients";
+import SignUp from "./components/SignUp";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginDashboard />} />
+      <Route path="/login-doctor" element={<Login />} />
+      <Route path="/login-patient" element={<LoginPatients />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+}
+
+export default App;
