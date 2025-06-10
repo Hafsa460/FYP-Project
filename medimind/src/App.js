a48b9297a67b7cbf22ec82c17839320d7519d790
Hafsa460 <221460@students.au.edu.pk>
2025-06-10 23:44:53 +0500

    Updated UI, added new features, and included a new CSS file

--- medimind/src/App.js
+++ medimind/src/App.js
@@ -1,20 +1,22 @@
 // src/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginDashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import LoginPatients from "./components/LoginPatients";
 import SignUp from "./components/SignUp";
-import NeuroDashboard from "./components/Neurologist/NeuroDashboard"; // <-- Import this
+import NeuroDashboard from "./components/Neurologist/NeuroDashboard";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginDashboard />} />
+      <Route path="/" element={<Navigate to="/dash" replace />} />
+
+      <Route path="/dash" element={<LoginDashboard />} />
       <Route path="/login-doctor" element={<Login />} />
       <Route path="/login-patient" element={<LoginPatients />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/neuro-dashboard" element={<NeuroDashboard />} /> {/* <-- Add this */}
+      <Route path="/neuro-dashboard/*" element={<NeuroDashboard />} />
     </Routes>
   );
 }
