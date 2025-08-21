c204ed0160c7fb70e94d4fbdc176427447438c9d
SiratSabir78 <siratsabir78@gmail.com>
2025-08-21 21:16:04 +0500

    Made the testreport and prescription page for the patient portal

--- medimind/frontend/src/App.js
+++ medimind/frontend/src/App.js
@@ -6,12 +6,13 @@ import LoginPatients from "../src/components/LoginPatients";
 import SignUp from "../src/components/SignUp";
 import LandingPage from "../src/components/LandingPage";
 import NeuroDashboard from "../src/components/Neurologist/NeuroDashboard";
-import ManageAppointment from '../src/components/Neurologist/ProfileManagement';
-import ViewPrescriptions from '../src/components/Neurologist/PrescriptionHistory';
-import TestHistory from '../src/components/Neurologist/VerifyReports';
+import ManageAppointment from "../src/components/Neurologist/ProfileManagement";
+import Prescription from "../src/components/Prescription";
+import TestHistory from "../src/components/Neurologist/VerifyReports";
+import TestReport from "../src/components/TestReport";
 import ProfileManagement from "../src/components/Neurologist/ProfileManagement";
-import PatientDashboard from '../src/components/PatientDashboard';
-import Navbar  from "./components/Navbar";
+import PatientDashboard from "../src/components/PatientDashboard";
+import Navbar from "./components/Navbar";
 import VerifySuccess from "./components/VerifySuccess";
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassowrd";
@@ -27,10 +28,11 @@ function App() {
       <Route path="/neuro-dashboard/*" element={<NeuroDashboard />} />
       <Route path="/PatientDashboard" element={<PatientDashboard />} />
       <Route path="/manage-appointment" element={<ManageAppointment />} />
-      <Route path="/view-prescriptions" element={<ManageAppointment />} />
+      <Route path="/view-prescriptions" element={<Prescription />} />
       <Route path="/test-history" element={<TestHistory />} />
       <Route path="/profile-management" element={<ProfileManagement />} />
-      <Route path="/verify-success" element={<VerifySuccess/>}/>
+      <Route path="/verify-success" element={<VerifySuccess />} />
+      <Route path="/testreport" element={<TestReport />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password/:token" element={<ResetPassword />} />
     </Routes>
@@ -38,4 +40,3 @@ function App() {
 }
 
 export default App;
-
