14ca3daa818bc6c50a3c72d8d5ef4b2c7aa549a9
SiratSabir78 <siratsabir78@gmail.com>
2025-09-16 21:15:13 +0500

    made some checks for appointments to work

--- medimind/backend/models/Appointment.js
+++ medimind/backend/models/Appointment.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
   doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor", required: true },
-  patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true },
-  date: { type: String, required: true }, // YYYY-MM-DD
-  time: { type: String, required: true }, // HH:mm
-});
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: String, required: true }, // yyyy-MM-dd
+  time: { type: String, required: true }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Appointment", appointmentSchema);
